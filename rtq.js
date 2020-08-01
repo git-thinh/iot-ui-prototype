7f142de1b46ba34fc07108bbc0128eec0f27be08
cuongnvgithub <68305632+cuongnvgithub@users.noreply.github.com>
2020-08-01 14:42:41 +0700

    rtq

---
+++ rtq.js
@@ -0,0 +1,26 @@
+﻿// load dat from home.json
+
+var app = new Vue({
+    el: '#data_rtd',
+    data: {
+        title: "RTQ",
+        ResultItems: {},
+    },
+    mounted: function () {
+        var _self = this;
+        var obj = fetch("rtq.json")
+            .then(response => response.json())
+            .then(jsonResponse => _self.ResultItems = jsonResponse);
+        console.log(obj);
+       
+
+    },
+    watch: {
+        innerModel: function (value) {
+        }
+    },
+    methods: {
+       
+
+    }
+});
